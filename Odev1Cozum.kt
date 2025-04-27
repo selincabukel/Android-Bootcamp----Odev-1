1adb450295c5d3a02d67fb8a506e8c934f48d273
selincabukel <selincabukell@gmail.com>
2025-04-27 09:05:03 +0300

    Android Bootcamp - Ödev 1 Çözüm
    Android Bootcamp Ödev 1 kapsamında Kotlin programlama dilinde temel veri tipleri kullanılarak değişkenler tanımlanmış ve çıktıları ekrana yazdırılmıştır.Kasım Adalan hocamıza çokça teşekkürler...

---
+++ Odev1Cozum.kt
@@ -0,0 +1,147 @@
+package com.example.androidbootcamp.odevler
+
+fun main(){
+
+    var sehir : String = "İstanbul"
+    println("Şehir $sehir'dur")
+
+    var ulke : String = "Türkiye"
+    println("Ülke $ulke'dir ")
+
+    val telefon: Long = 5067899989
+    println("Telefon numarası $telefon'dur")
+
+    val postakodu = 34050
+    println("Posta kodu $postakodu'dur ")
+
+    val email : String = "selincabukell@gmail.com"
+    println("E-mail adresi $email'dir")
+
+    var meslek = "Öğrenci"
+    println("Mesleği $meslek'dir")
+
+    var stokmiktarı = 4567890
+    println("Ulaşabileceğiniz stok miktarı $stokmiktarı'dır ")
+
+    var musteriadi = "Kasım Adalan"
+    println("Müşteri adı $musteriadi'dır")
+
+    var bakiye = 4567890323434
+    println("Kalan bakiye $bakiye'dir")
+
+    val dogumgunu =  "19-01-2001"
+    println("Doğum günü : $dogumgunu'dir")
+
+    var maas : Int = 300000
+    println("Maaşı $maas eurodur")
+
+    var medenidurum = "Bekar"
+    println("Medeni durumu $medenidurum'dır")
+
+    var ürünyorum : String = "Ürün fena değil ama çok mükemmel olduğunu söyleyemem..."
+    println("Ürün yorumu şöyledir: $ürünyorum")
+
+    val ödemetarihi = "19-05-2025"
+    println("Ödeme tarihi $ödemetarihi'dir")
+
+    var ödeme: String = "nakit"
+    println("Ödeme $ödeme'tir")
+
+    var siparis_adedi = 5
+    println("Sipariş adedi $siparis_adedi'tir")
+
+    val araba_modeli = "Range Rover"
+    println("Araba modeli $araba_modeli'dir")
+
+    val kitap_adi = "Beyaz Zambaklar Ülkesinde"
+    println("Kitap adı $kitap_adi'dır")
+
+    val yayinlanma_tarihi = "1923"
+    println("Yayınlanma tarihi $yayinlanma_tarihi'dir")
+
+    var indirim_miktari = 45
+    println("İndirim miktarı $indirim_miktari tl'dir")
+
+    var oda_sayisi = 3
+    println("Oda sayısı $oda_sayisi'dır")
+
+    val enlem: Double = 40.879326
+    print("Enlem $enlem'dır")
+
+    val boylam : Double = 29.258135
+    println("Boylam $boylam'dır")
+
+    val ürün_adi : String = "Hamburger"
+    println("Ürün adı $ürün_adi'dır")
+
+    var yemek_fiyati : Double = 320.99
+    println("Ürün fiyatı $yemek_fiyati'dır")
+
+    var marka = "Unilever"
+    println("Marka $marka'dır")
+
+    val müzik_adi = "Üç Kalp"
+    println("Müzik adı $müzik_adi'tir")
+
+    val video_süresi = 3.51
+    println("Video süresi $video_süresi'dir")
+
+    var ürün_puani = 3.56
+    println("Ürün puanı $ürün_puani'dır")
+
+    val resimadi = "Mona Lisa"
+    println("Resim adı $resimadi'dır")
+
+    val dosya_formati = "XML"
+    println("Dosya formatı $dosya_formati'dir")
+
+    var renk = "Kırmızı"
+    println("Renk : $renk sana $renk çok yakışıyorrrr")
+
+    var renk_kodu = 4294901760F
+    println("Renk kodu $renk_kodu'dır")
+
+    val telefon_modeli = "Xiaomi Note 13 Pro"
+    println("Telefon modeli $telefon_modeli'dur")
+
+    val ekran_boyutu = "6.67 inç"
+    println("Ekran boyutu $ekran_boyutu'tir")
+
+    val agirlik = "188 gr"
+    println("Ağırlığı $agirlik'dır ")
+
+    val ulusalgun = "30 Ağustos Zafer Bayramı"
+    println("Ulusal günlerimizden biri $ulusalgun'dır")
+
+    var tatil_gunu = "01-07-2025"
+    println("Sınavların bittiği gün $tatil_gunu'tir. Şimdidem iyi tatillerrr :) ")
+
+    var rezervasyon_tarihi = "02.07.2025"
+    println("Rezervasyon tarihi $rezervasyon_tarihi'tir. Bol bol güneşin tadını çıkar")
+
+    val sokak_adi = "Çiçek sokak"
+    println("Bulunduğunuz sokağın adı $sokak_adi'tır")
+
+    val otobüs_hatti = "50V"
+    println("Kullandığınız otobüs hattı $otobüs_hatti'dir")
+
+    var kalan_dakika = 1.05
+    println("Kalan dakika $kalan_dakika'dır")
+
+    val takip_kodu : String= "AB345678CD44"
+    println("Takip kpdunuz $takip_kodu'dur")
+
+    var kupon_suresi = 14
+    println("Kupon süresi $kupon_suresi gündür")
+
+    val kupon_kodu = "WcHIbKl1H3"
+    println("Kullanabileceğiniz kupon kodunuz $kupon_kodu'dur")
+
+    val fatura_adresi = "Feriha mah. Emir'in yolu sk no:3/5 daire:13"
+    println("Fatura adresiniz : $fatura_adresi")
+
+
+
+
+
+}
